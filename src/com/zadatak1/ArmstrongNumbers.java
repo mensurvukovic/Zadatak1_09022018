3ebc6cf9506562a99b83f9b5a0b603080cb2b01f
Mensur Vukovic <mensur.v@hotmail.com>
2018-02-09 14:31:05 +0100

    Uradjena klasa Armstrong

--- src/com/zadatak1/ArmstrongNumbers.java
+++ src/com/zadatak1/ArmstrongNumbers.java
@@ -2,7 +2,27 @@ package com.zadatak1;
 
 public class ArmstrongNumbers {
 	
-	public void armstrong(int armsNum){
+	public static void armstrong(){
+		
+		int zbir = 0;
+		
+		for (int i=1; i<1000; i++){
+			
+			int broj = i;
+			
+			while (broj>0){
+			
+			int ostatak=broj%10;
+			broj/=10;
+			zbir+=Math.pow(ostatak, 3);
+			
+		}
+			if(zbir == i){
+			System.out.println(i);
+			
+			}
+			zbir=0;
+		}
 		
 	}
 
