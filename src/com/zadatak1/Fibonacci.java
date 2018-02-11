55d014396c1f6b7a1385db9e83f5003ec29c0690
mensurvukovic <36190241+mensurvukovic@users.noreply.github.com>
2018-02-11 18:09:34 +0100

    Add files via upload

--- src/com/zadatak1/Fibonacci.java
+++ src/com/zadatak1/Fibonacci.java
@@ -1,9 +1,24 @@
 package com.zadatak1;
 
 public class Fibonacci {
-	
-	public void fibonacci(int fibonacci){
-		
-	}
 
+	public static void fibonacci() {
+
+		long[] fibonaciNiz = new long[100];
+
+		for (int i = 0; i <= fibonaciNiz.length - 1; i++) {
+
+			if (i < 2) {
+				int clan = i;
+				fibonaciNiz[i] = clan;
+			} else {
+				long clan = fibonaciNiz[i - 1] + fibonaciNiz[i - 2];
+				fibonaciNiz[i] = clan;
+			}
+
+		}
+		for (int i = 0; i <= fibonaciNiz.length - 1; i++) {
+			System.out.println(fibonaciNiz[i]);
+		}
+	}
 }
